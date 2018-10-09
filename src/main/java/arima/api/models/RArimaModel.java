5553c68516419ae2eb55dc77e51a272c24fe7650
Owen Sims <owen.sims@cyberlytic.com>
2018-10-09 15:10:22 +0100

    Add initial RArima model

---
+++ src/main/java/arima/api/models/RArimaModel.java
@@ -0,0 +1,14 @@
+package arima.api.models;
+
+public class RArimaModel {
+	
+	private Integer[] tsdata;
+
+	public Integer[] getTSData() {
+		return tsdata;
+	}
+
+	public void setTSData(Integer year) {
+		this.tsdata = tsdata;
+	}
+}
