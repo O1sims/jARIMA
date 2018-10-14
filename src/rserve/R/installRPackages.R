26964b5f60bb924766f5516048567d59a305545b
Owen Sims <owen.sims@cyberlytic.com>
2018-10-15 00:16:12 +0100

    Install R packages correctly

--- src/rserve/R/installRPackages.R
+++ src/rserve/R/installRPackages.R
@@ -17,29 +17,11 @@ cranRepo <- 'http://cran.us.r-project.org'
 
 # Install devtools if not installed already
 # Required for installing versions
-if (!("devtools" %in% installed.packages())) {
-  utils::install.packages(
-    "devtools",
-    repos = cranRepo)
-}
-
-# CRAN packges to install with versions
-packageList <- list(
-  "Rserve" = "1.7-3",
-  "RSclient" = "0.7-3",
-  "forecast" = "8.3")
-
-# Generate an array of packages that have not already been installed
-newPackages <- subset(
-  x = names(packageList),
-  subset = !(names(packageList) %in% installed.packages()))
-
-# Install any new packages from CRAN
-if (length(newPackages) > 0) {
-  for (package in newPackages) {
-    devtools::install_version(
-      package = package,
-      version = packageList[[package]],
+packageList <- c("Rserve", "RSclient", "forecast")
+for (package in packageList) {
+  if (!(package %in% installed.packages())) {
+    utils::install.packages(
+      package,
       repos = cranRepo)
   }
 }
