8449c3e61613309e5c3247c845645eb36b92fa64
Owen Sims <owen.sims@cyberlytic.com>
2018-10-15 08:58:47 +0100

    Put perform ARIMA function into the workspace initiation

--- src/rserve/R/initiateArimaWorkspace.R
+++ src/rserve/R/initiateArimaWorkspace.R
@@ -7,3 +7,14 @@ initiateArimaWorkspace <- function() {
     paste0(6311) %>%
     print()
 }
+
+
+performArima <- function(tsData) {
+  fut <- tsData %>%
+    auto.arima() %>%
+    forecast(1) %>%
+    as.data.frame()
+  return(c(
+    fut$`Hi 95` %>% as.double(),
+    fut$`Lo 95` %>% as.double()))
+}
