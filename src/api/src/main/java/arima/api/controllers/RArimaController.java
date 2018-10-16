2680d4aa611a06cadd8587fbedf9470d1214930d
Owen Sims <owen.sims@cyberlytic.com>
2018-10-16 17:01:06 +0100

    Crate RServe connection and run ARIMA analysis in the Controller itself

--- src/api/src/main/java/arima/api/controllers/RArimaController.java
+++ src/api/src/main/java/arima/api/controllers/RArimaController.java
@@ -1,39 +1,47 @@
 package arima.api.controllers;
 
 import arima.api.models.RArimaModel;
-import arima.api.services.RArimaService;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.rosuda.REngine.REXPMismatchException;
-import org.rosuda.REngine.REngineException;
+import org.rosuda.REngine.Rserve.RConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 
 @RestController
 @RequestMapping("/r-arima")
 public class RArimaController {
 	
+	private RConnection connection;
+	
+	@Value("${rserve.port}") @NotNull private int port;
+	@Value("${rserve.hostname}") @NotNull private String hostname;
+	@Value("${rserve.production}") @NotNull private boolean production;
+	
 	private final Logger LOGGER = LoggerFactory.getLogger(RArimaController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public void calculateRArima(
-			@Valid @RequestBody RArimaModel rArima,
-			RArimaService rArimaService) 
-					throws REXPMismatchException, REngineException {
-		
-		LOGGER.info("Parsing out TS Data");
-		int[] data = rArima.getTSData();
-		
-		LOGGER.info("Sending off to arima function...");
-		double[] something = rArimaService.performRArima(data);
-		
-		this.LOGGER.info(something.toString());
-
-  }
+	@RequestMapping(
+			value = "/", 
+			method = RequestMethod.POST)
+	public double[] calculateRArima(
+			@Valid @RequestBody RArimaModel rArima)
+					throws Exception {
+		LOGGER.info("Connecting to Rserve: {}:{}", this.hostname, this.port);
+        this.connection = new RConnection(this.hostname, this.port);
+        this.connection.assign("tsData", rArima.getTSData());
+        LOGGER.info("Evaluating time-series data...");
+        this.connection.voidEval("dffv <- data.frame(forecast::forecast(forecast::auto.arima(tsData), 1))");
+        double[] out = {
+        		this.connection.eval("dffv$Hi.95").asDouble(),
+            	this.connection.eval("dffv$Lo.95").asDouble()
+        };
+        return out; 
+	}
 }
