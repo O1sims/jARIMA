81e81f46adeb81845a20658bef016cdf6d70e986
Owen Sims <owen.sims@cyberlytic.com>
2018-10-16 17:00:11 +0100

    Remove initial call from the Application to initialise the RServe connection

--- src/api/src/main/java/arima/api/Application.java
+++ src/api/src/main/java/arima/api/Application.java
@@ -1,35 +1,13 @@
 package arima.api;
 
-import arima.api.configuration.RServeConfig;
-import arima.api.services.RArimaService;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class Application {
 	
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Bean
-    public CommandLineRunner commandLineRunner(
-    		ApplicationContext ctx,
-    		RServeConfig rServeConfig,
-    		RArimaService rArimaService) {
-        return args -> {
-        	LOGGER.info("Setting up RServe environment...");
-        	rArimaService.initRServeWorkspace(rServeConfig);
-        };
-	};
 }
