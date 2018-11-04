14bcb04000ddabfaffba138bc8dc26a9162915dd
Owen Sims <owen.sims@cyberlytic.com>
2018-11-04 22:01:36 +0000

    Add some auto aspect to this

--- src/api/src/main/java/arima/api/analytics/timeseries/arima/Arima.java
+++ src/api/src/main/java/arima/api/analytics/timeseries/arima/Arima.java
@@ -1,5 +1,10 @@
 package arima.api.analytics.timeseries.arima;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import arima.api.analytics.timeseries.arima.struct.ArimaModel;
 import arima.api.analytics.timeseries.arima.struct.ArimaParams;
 import arima.api.analytics.timeseries.arima.struct.ForecastResult;
@@ -19,32 +24,67 @@ public final class Arima {
      * @param data UNMODIFIED, list of double numbers representing time-series with constant time-gap
      * @param forecastSize integer representing how many data points AFTER the data series to be
      *        forecasted
-     * @param params ARIMA parameters
      * @return a ForecastResult object, which contains the forecasted values and/or error message(s)
      */
     public static ForecastResult forecast_arima(final double[] data, final int forecastSize) {
 
         try {
-        	final ArimaParams paramsForecast = new ArimaParams(3, 0, 3, 1, 1, 0, 0);
-            // estimate ARIMA model parameters for forecasting
-            final ArimaModel fittedModel = ArimaSolver.estimateARIMA(
-                paramsForecast, data, data.length, data.length + 1);
-
-            // compute RMSE to be used in confidence interval computation
-            final double rmseValidation = ArimaSolver.computeRMSEValidation(
-                data, ForecastUtil.testSetPercentage, paramsForecast);
-            fittedModel.setRMSE(rmseValidation);
-            final ForecastResult forecastResult = fittedModel.forecast(forecastSize);
-
-            // populate confidence interval
-            forecastResult.setSigma2AndPredicationInterval(fittedModel.getParams());
+        	// establish array to store forecast results
+        	ForecastResult currentModel = null;
+        	
+        	// initial models (Hyndman and Khandakar, 2008:11)
+        	// for non-seasonal data
+        	int[] params = {2, 2, 0, 0, 1, 0, 0, 1};
+        	List<Integer> list = Arrays
+        			.stream(params)
+        			.boxed()
+        			.collect(Collectors.toList());
+        	
+        	// loop over potential models
+        	for (int x = 0; x <=(params.length-2)/2; x++) {
+        		currentModel = forecast(
+        				currentModel, data, forecastSize, list.get(x*2), list.get((x*2)+1));
+        	}
 
             // successfully built ARIMA model and its forecast
-            return forecastResult;
+            return currentModel;
 
         } catch (final Exception ex) {
             // failed to build ARIMA model
             throw new RuntimeException("Failed to build ARIMA forecast: " + ex.getMessage());
         }
+        
+    }
+    
+    private static ForecastResult forecast(ForecastResult currentModel, final double[] data, 
+    		final int forecastSize, int p, int q) {
+    	
+    	// set the initial parameters for (p, d, q, P, D, Q, m)
+		final ArimaParams paramsForecast = new ArimaParams(
+				p, 0, q, 1, 0, 1, 1);
+    	
+        // estimate ARIMA model parameters for forecasting
+        final ArimaModel fittedModel = ArimaSolver.estimateARIMA(
+            paramsForecast, data, data.length, data.length + 1);
+
+        // compute RMSE to be used in confidence interval computation
+        final double rmseValidation = ArimaSolver.computeRMSEValidation(
+            data, ForecastUtil.testSetPercentage, paramsForecast);
+        
+        if (currentModel == null || rmseValidation < currentModel.getRMSE()) {
+        	// set the RMSE
+            fittedModel.setRMSE(rmseValidation);
+            
+            // run forecast model
+        	final ForecastResult forecastResult = fittedModel.forecast(forecastSize);
+            
+            // populate confidence interval
+            forecastResult.setSigma2AndPredicationInterval(fittedModel.getParams());
+            
+            // add to forecast results
+            return forecastResult;
+        }
+        
+        return currentModel;
     }
 }
